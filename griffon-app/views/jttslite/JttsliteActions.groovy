1e3634e75656b948d6850d613b544d3fecb8116e
Davide Cavestro <davide.cavestro@gmail.com>
2012-10-15 09:21:41 +0200

    Restarted from scratch with "jumpstart" archetype

---
+++ griffon-app/views/jttslite/JttsliteActions.groovy
@@ -0,0 +1,30 @@
+package jttslite
+
+import javax.swing.KeyStroke
+
+actions {
+    action(saveAction, enabled: false)
+    action(saveAsAction, enabled: false)
+    action(undoAction, enabled: false)
+    action(redoAction, enabled: false)
+    action(cutAction, enabled: false)
+    action(copyAction, enabled: false)
+    action(pasteAction, enabled: false)
+    action(deleteAction,
+            accelerator: KeyStroke.getKeyStroke(app.getMessage('application.action.Delete.accelerator', 'meta DELETE')),
+            enabled: false
+    )
+
+    action(
+            id: "startWorkLogAction",
+            name: "Start",
+            enabled: bind {!model.inProgress},
+            closure: controller.&startProgress
+    )
+    action(
+            id: "stopWorkLogAction",
+            name: "Stop",
+            enabled: bind {model.inProgress},
+            closure: controller.&stopProgress
+    )
+}
---
+++ griffon-app/views/jttslite/JttsliteContent.groovy
@@ -0,0 +1,7 @@
+package jttslite
+
+panel {
+    migLayout layoutConstraints: 'fill'
+    label('Your new application has been created!', constraints: 'center, wrap')
+    label(icon: imageIcon('/griffon-icon-256x256.png'), constraints: 'center')
+}
